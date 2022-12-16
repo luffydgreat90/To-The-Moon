166d2e9539a90949f7dfc2049ad85c33a4396788
marlon ansale <luffydgreat90@gmail.com>
2022-12-16 17:06:35 +0000

    Fix Mock issues.

--- ToTheMoon/ToTheMoon/Common/Model/Mock/MockCoin.swift
+++ ToTheMoon/ToTheMoon/Common/Model/Mock/MockCoin.swift
@@ -7,10 +7,18 @@
 
 import Foundation
 
-var mockCoin: Coin = {
-    Coin(id: "BTC", symbol: "BTC", name: "Bitcoin", image: URL(string:"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579")!, currentPrice: 23.22, lastUpdated: Date(), marketCapRank: 1, priceChange24h: 232.33, high24h: 25.33, low24h: 10.22, priceChangePercentage24h: 0.10)
-}()
 
 var mockCoinViewModel: CoinViewModel = {
-    CoinViewModel(coin: mockCoin)
+    CoinViewModel(id: "BTC",
+                  symbol: "BTC",
+                  name: "Bitcoin",
+                  image: URL(string:"https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579")!,
+                  imageSmall: URL(string:"https://assets.coingecko.com/coins/images/1/small/bitcoin.png?1547033579")!,
+                  price: "$23.22",
+                  lastUpdated: Date(),
+                  marketCapRank: "Rank #1",
+                  high24h: "$25.33",
+                  low24h: "$10.22",
+                  priceChangePercentage24h: "$0.10",
+                  statusColor: .green)
 }()
